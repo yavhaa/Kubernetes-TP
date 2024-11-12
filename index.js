20e9f950b50e15dfd616172e026934538ab2562c
Jawher <jawher.jandoubi1@gmail.com>
2024-11-12 15:22:44 +0100

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Kubernetes!, #UPDATED');
+  res.send('Hello, Kubernetes! aabbdd, #UPDATED');
 });
 
 app.listen(port, () => {
